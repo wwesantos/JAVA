f9ee16176b8364dab49a1d2f6a6e53db23b78816
Waldeilson Santos <wesantos@br.ibm.com>
2019-05-23 11:22:06 -0300

    Revert "Revert "Java Server Socket with Threads""
    This reverts commit 1cf64232d3a6fd4afc9ee140f5ea2400c539db22.

---
+++ ClientServerWithThread/src/server/ClientHandler.java
@@ -0,0 +1,68 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.List;
+
+import domain.Employee;
+import repo.EmplyeeDAO;
+
+//ClientHandler class 
+public class ClientHandler extends Thread { 
+	final DataInputStream dis; 
+	final ObjectOutputStream dos; 
+	final Socket s; 
+	
+
+	// Constructor 
+	public ClientHandler(Socket s, DataInputStream dis, ObjectOutputStream dos) { 
+		this.s = s; 
+		this.dis = dis; 
+		this.dos = dos; 
+	} 
+
+	@Override
+	public void run() { 
+		String filterBy; 
+		while (true) { 
+			try {
+				// receive the answer from client 
+				filterBy = dis.readUTF(); 
+				
+				if(filterBy.equals("exit")) { 
+					closeConnection();
+					break; 
+				} 
+				
+				List<Employee> result = EmplyeeDAO.getBy(filterBy);
+				dos.writeObject(result);
+				
+			} catch (IOException e) { 
+				e.printStackTrace(); 
+			} 
+		} 
+		
+		closeReseources(); 
+	}
+
+	private void closeConnection() throws IOException {
+		System.out.println("Client " + this.s + " sends exit..."); 
+		System.out.println("Closing this connection."); 
+		this.s.close(); 
+		System.out.println("Connection closed");
+	}
+
+	private void closeReseources() {
+		try{ 
+			this.dis.close(); 
+			this.dos.close(); 
+			
+		}catch(IOException e){ 
+			e.printStackTrace(); 
+		}
+	} 
+	
+
+} 
